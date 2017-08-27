d2b63bd0e8c60530d633a788f05551fff10b5813
Matthew Manela <mmanela@gmail.com>
2017-08-27 14:40:30 -0700

    repro

---
+++ EdgeTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using EdgeJs;
+using System;
+using System.Threading.Tasks;
+
+namespace EdgeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            Test().Wait();
+        }
+
+        public static async Task Test()
+        {
+            var onMessage = (Func<object, Task<object>>)( (message) =>
+            {
+                Console.WriteLine($"Got this message: {message}");
+                return Task.FromResult((object)"ok");
+            });
+
+            var func = Edge.Func(@"
+            return function (params, callback) {
+
+                params.onMessage('!!_!! This is cool', function(error, result) {});
+                callback(null, 'Node.js welcomes ' + params.message);
+            }
+        ");
+            
+            Console.WriteLine(await func(new { message = "YES", onMessage = onMessage }));
+        }
+    }
+}
---
+++ EdgeTest/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EdgeTest
+{
+    public class Test
+    {
+        [Fact]
+        public void Litmus()
+        {
+            Program.Run();
+
+            Assert.True(true);
+        }
+    }
+}
